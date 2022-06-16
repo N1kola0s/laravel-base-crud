242dc3c1a5ede6bc0b2901024d0bd266b72e885b
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-06-16 17:15:25 +0200

    add table add column in table

--- database/migrations/2022_06_16_143907_create_comics_table.php
+++ database/migrations/2022_06_16_143907_create_comics_table.php
@@ -15,6 +15,13 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->text('description')->nullable();
+            $table->string('thumb');
+            $table->decimal('price', 5,2)->unsigned()->nullable();
+            $table->string('series', 100)->nullable();
+            $table->string('sale_date', 20)->nullable();
+            $table->string('type', 20)->nullable();
             $table->timestamps();
         });
     }
