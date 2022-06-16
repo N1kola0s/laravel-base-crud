25ff4f1c128f2a3a8f908099e563e50ee99e6f14
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-06-16 16:40:58 +0200

    add Model add Factory create Migration create Seeder create Controller

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,8 @@
+
+
+<footer id="site_footer">
+    <h4>
+        &copy; by Nicola Migliore
+    </h4>
+</footer>
+<!-- /#site_footer -->
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,7 @@
+<header>
+  <nav class="nav justify-content-center">
+    <a class="nav-link" href="{{route('home')}}">Home</a>
+    <a class="nav-link" href="#">prova</a>
+  </nav>
+</header>
+<!-- /#site_header -->
